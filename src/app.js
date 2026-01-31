add2c60ad11a2e5491d6eba6eb1dd8b6e2d07877
m1v8a <245783116+m1v8a@users.noreply.github.com>
2026-01-31 13:50:00 +0800

    feat: Add CRUD for todos

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
 import "./style.css";
+import App from "./modules/App.js";
+
+
+App.createTodo({
+    title: "example", note: "a"
+});
+
+
+console.log(App.getTodos());
+
+
---
+++ src/class/Todo.js
@@ -0,0 +1,10 @@
+export default class Todo {
+    constructor({title, note, dueDate, priority}) {
+        this.title = title;
+        this.note = note;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        // this.id = crypto.randomUUID();
+        this.id = "abc";
+    }
+}
